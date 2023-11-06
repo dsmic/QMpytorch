f69dc7cb0f274e3065098b0725e4235e6057d8d3
detlef <dschmicker@physik.de>
2023-11-06 17:24:49 +0100

    Entanglement in sample script

--- QMpytorch.py
+++ QMpytorch.py
@@ -20,8 +20,8 @@ vmap_chunk_size = 100000  # 2**13
 print("vmap_chunk_size", vmap_chunk_size)
 
 N_Int_Plot = 5000
-N_Int_Points = 10000000
-do_paired = True
+N_Int_Points = 1000000
+do_paired = False
 
 set_up_backend("torch", data_type="float64")
 # set_log_level("INFO")
@@ -49,10 +49,20 @@ V_strength = 1.0
 
 # the following functions are used to calculate the integration ranges
 def calc_int_electron(ppp):
+    """
+    ppp: tensor of parameters
+
+    result is the + - range of the electron integration
+    """
     return (ppp[0] * 1.5 + ppp[1]) * 1.2
 
 
 def calc_int_nuclei(ppp):
+    """
+    ppp: tensor of parameters
+
+    result is the + - range of the nuclei integration
+    """
     return 1.3 * ppp[1]
 
 
@@ -76,6 +86,12 @@ for i in range(nNuclei):
 
 
 def CutRange(x, r, f):
+    """
+    helper to be used in wave functions, cuts an range -r to +r with some parameter f for the hardness of the cut
+    x: position
+    r: + - range
+    f: hardness of the cut
+    """
     return torch.sigmoid((x+r)*f)*(1-torch.sigmoid((x-r)*f))
 
 
@@ -83,12 +99,22 @@ q = torch.tensor([-1]*nElectrons + [q_nuclei]*nNuclei)
 
 
 def damp(x, w):
+    """
+    typical gaussian form to be used in wave functions
+    x: position
+    w: width parameter
+    """
     return torch.exp(-(x/w)**2)
     # ret = torch.sigmoid(x/w - w) * torch.sigmoid(-x/w - w)
     # return ret / torch.sigmoid(-w)**2
 
 
 def wf_form(x, w):
+    """
+    typical gaussian form to be used in wave functions
+    x: position
+    w: width parameter
+    """
     return torch.exp(-(x/w)**2)
     # ret = torch.sigmoid(x/w * 2.0) * torch.sigmoid(-x/w * 2.0)
     # return ret * 4.0
@@ -98,12 +124,19 @@ def wf_form(x, w):
 # V[x_, y_, z_] := 1/Sqrt[x^2 + y^2 + z^2]
 # Plot[{NIntegrate[V[x, y, z], {y, -0.5, 0.5}, {z, -0.5, 0.5}], 1/(Abs[x/1.2] + 0.28)}, {x, -3, 3}]
 def V(dx):
+    """
+    dx: distance of two particles
+    retuns the potential from a toy function, not exactly coulomb ...
+    """
     # return 1.0 / (torch.abs(dx / 1.2) + 0.28)    # Coulomb potential part integrated from 3D
     return torch.exp(-dx**2)            # Easy to integrate potential
 
 
 def Vpot(xinp):
-    """Potential energy"""
+    """
+    xinp: positions
+    returns the Potential
+    """
     x = xinp + offsets
     x1 = x.reshape(-1, 1)
     x2 = x.reshape(1, - 1)
@@ -114,6 +147,11 @@ def Vpot(xinp):
 
 
 def Epot(wf, x):
+    """
+    wf: wave function takes only x
+    x: position
+    returns the value of the potential energy integrand
+    """
     return (torch.conj(wf(x)) * Vpot(x) * wf(x)).real
 
 
@@ -134,6 +172,11 @@ def Epot_plot(wf, x, plot_pos):
 
 
 def H_single(wf, x):
+    """
+    wf: wave function takes only x
+    x: position
+    returns the value of the H integrand
+    """
     # <see cref="file://./Docs/PartIntMulti.jpg"/>
     if j is None:
         gg = torch.func.grad(lambda x: wf(x).real)(x)
@@ -145,6 +188,13 @@ def H_single(wf, x):
 
 
 def H(wf, x):
+    """
+    wf: wave function takes only x
+    x: position
+
+    vectorized function of H_single
+    returns the value of the H integrand
+    """
     gg = vmap(lambda x: H_single(wf, x), chunk_size=vmap_chunk_size)(x)
     return gg
 
@@ -170,6 +220,10 @@ sf_array[1, 0, 0] = -1
 
 
 def testwf(ppp, xx):
+    """
+    ppp: tensor of the parameters for the wave function
+    xx: position
+    """
     def sf(x):
         x = x[:nElectrons]
         res = sf_array[tuple(x)]
@@ -196,6 +250,11 @@ def testwf(ppp, xx):
 
 
 def Norm(wf, x):
+    """
+    wf: wave function, takes only a x tensor as input
+    x: x tensor to calculate
+    returns the spacial density
+    """
     return (torch.conj(wf(x)) * wf(x)).real
 
 
@@ -203,7 +262,7 @@ def Norm(wf, x):
 for i in range(nNuclei):
     offsets[i+nElectrons] = ppp[0] * (i - nNuclei//2)
 
-# create permutations
+# create permutations of identical particles, only the electrons are permuted, the nuclei not at the moment.
 perms = []
 perms_p = []
 for i in permutations(list(range(nElectrons))):
@@ -221,6 +280,12 @@ print('perms.shape', perms.shape)
 
 
 def plotwf(ppp, plot_pos, where):
+    """
+    plot function for wave function
+    ppp: parameters
+    plot_pos: which position to plot
+    where: on which subplot to plot
+    """
     global offsets
     for i in range(nNuclei):
         offsets[i+nElectrons] = ppp[0] * (i - nNuclei//2)
@@ -261,6 +326,10 @@ if replot:
 
 
 def doplot(pinp):
+    """
+    pinp: parameters
+    plot some wave functions and potential energy
+    """
     global fig, axs
     ppp = torch.tensor(pinp)
     if not replot:
@@ -293,6 +362,11 @@ map_H = None
 
 
 def doIntegration(pinp, seed=None, N=None):
+    """
+    pinp: list (or tensor) of the parameters
+    seed: random seed
+    N: Number of integration points
+    """
     global plotcounter, map_Norm, map_H
     if N is not None:
         N_Int_Points_loc = N
@@ -327,12 +401,72 @@ def doIntegration(pinp, seed=None, N=None):
     if Normvalue < 0.0001:
         print("Normvalue too small", Normvalue)
         raise Exception("Normvalue too small, probable zero wave function (e.g. due to antisymmetry)")
-    retH = integral_value/Normvalue
+    retH = integral_value / Normvalue
     print("              H", "{:.5f}".format(float(retH.cpu())), ppp.cpu().numpy(), "{:.2f}".format(time.time() - start), "(" + "{:.2f}".format(plottime) + ")", 'raw Norm integral value', "{:.5f}".format(Normvalue), 'seed', seed)
     return retH.cpu().numpy()
 
 
+def get_ratio(wf, x, kk):
+    """
+    wf: wavefunction
+    xx: The variable, nParticles-1 nParticles-1 and one variable at pos kk
+    kk: position of the extra variable
+    """
+    xx1 = torch.cat((x[:kk], x[-2:-1], x[kk:nParticles-1]))
+    xx2 = torch.cat((x[:kk], x[-1:], x[kk:nParticles-1]))
+    t1 = Norm(wf, xx1)
+    t2 = Norm(wf, xx2)
+    return t1, t2
+
+
+def do_entangled_std(pinp, kk, seed=None, N=None):
+    """
+    calculate entanglement of one position against all others
+    pinp: parameters
+    kk: which position
+    N: number of random points to use
+    """
+    ppp = torch.tensor(pinp)
+    if N is not None:
+        N_Int_Points_loc = N
+    else:
+        N_Int_Points_loc = N_Int_Points
+    N_Int_Points_loc = int(np.sqrt(N_Int_Points_loc))
+    IntElectron = [[-calc_int_electron(ppp), calc_int_electron(ppp)]]
+    IntNuclei = [[-calc_int_nuclei(ppp), calc_int_nuclei(ppp)]]
+    intdomain = IntElectron*nElectrons+IntNuclei*nNuclei
+    intdomain_d = intdomain[:kk] + intdomain[kk+1:]
+    stds = []
+    means = []
+    densities = []
+    for _ in range(N_Int_Points_loc):
+        rand_tensor = torch.rand(N_Int_Points_loc, len(intdomain_d)) * (torch.tensor(intdomain_d)[:, 1]-torch.tensor(intdomain_d)[:, 0]) + torch.tensor(intdomain_d)[:, 0]
+        rand_same = torch.rand(1, 2) * (torch.tensor(intdomain[kk])[1] - torch.tensor(intdomain[kk])[0]) + torch.tensor(intdomain[kk])[0]
+        rand_tensor = torch.cat((rand_tensor, rand_same.repeat((N_Int_Points_loc, 1))), dim=1)
+        t1, t2 = vmap(lambda y: get_ratio(lambda x: testwf(ppp, x), y, kk), chunk_size=vmap_chunk_size)(rand_tensor)
+        # res = torch.log(t1 / (t2+1E-10))
+        # stds.append(res.std())
+        # means.append(res.mean())
+        # print('mean', res.mean(), 'std', res.std())
+        res = torch.log(t1 / (t2+1E-10))
+        dens_mean = (t1+t2).sum()
+        res_mean = (res * (t1+t2)).sum() / dens_mean
+        res_std = (res**2 * (t1+t2)).sum() / dens_mean - res_mean**2
+        stds.append(res_std)
+        means.append(res_mean)
+        densities.append(dens_mean)
+        # print('mean', res_mean, 'std', res_std)
+
+    # res_mean = np.array(means).mean()
+    # res = np.array(stds).mean()
+    res_mean = (np.array(means)*np.array(densities)).sum() / (np.array(densities)).sum()
+    res = (np.array(stds)*np.array(densities)).sum() / (np.array(densities)).sum()
+    print('entanglement measure of', kk, 'full mean', res_mean, 'std', res)
+    return res, res_mean
+
+
 if do_paired:
+    # this checks for the integration error
     # doIntegration(ppp)  # activate this to check, if integration with the same seed gives the same result
     calc_std = []
     for s in range(5):
@@ -343,6 +477,14 @@ else:
     doIntegration(ppp)
 
 starttime = time.time()
+
+
+def show_entanglement(ppp):
+    for k in range(nParticles):
+        do_entangled_std(ppp, k)
+
+
+show_entanglement(ppp)
 
 # This very robust optimizer is not very fast in my tests, at least two orders of magnitude slower than SPSA
 # ret = minimizeCompass(doIntegration, x0=ppp, deltainit=0.6, deltatol=0.1, bounds=[[0.01, 20.0]] * (ppp.shape[0]), errorcontrol=do_errorcontrol, funcNinit=30, feps=0.003, disp=True, paired=True, alpha=0.2)
@@ -363,6 +505,8 @@ for k in range(len(ret.x)):
 print("integration with higher precision")
 doIntegration(ret.x, seed=None, N=10 * N_Int_Points)
 doIntegration(ret.x, seed=None, N=100 * N_Int_Points)
+
+show_entanglement(ret.x)
 
 if do_plot_every is not None:
     doplot(torch.tensor(ret.x))
